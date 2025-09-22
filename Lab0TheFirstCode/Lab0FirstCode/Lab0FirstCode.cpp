916194340ceec5797b07f5fd3b2e8fb89327d241
Глеб <glebruban2007@gmail.com>
2025-09-22 22:39:17 +0200

    ADD: lab 0

---
+++ Lab0TheFirstCode/Lab0FirstCode/Lab0FirstCode.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+int main()
+{
+	std::cout << "The first code in C++ for lab0" << std::endl;
+	return 0;
+}
---
+++ Lab0TheThirdCode/Lab0TheThirdCode/Lab0TheThirdCode.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+int main()
+{
+	std::cout << "The ending test code" << std::endl;
+	std::cout << "That's all!" << std::endl;
+	std::cout << "ALL!!!" << std::endl;
+}
